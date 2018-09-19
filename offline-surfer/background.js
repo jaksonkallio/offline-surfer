2a62c7ba4a79f818ffc5cba2c53c8f685518ee4a
Jakson Kallio <jakson@protonmail.com>
2018-09-19 13:32:31 -0500

    Correctly refresh on error

--- offline-surfer/background.js
+++ offline-surfer/background.js
@@ -6,10 +6,32 @@ function startup(){
 	console.log("Offline Surfer is active");
 }
 
-function statusCallback(status){
-	console.log(status.url);
+function retry(tab_id){
+	console.log("Retrying page load");
+
+	browser.tabs.get(tab_id).then(function (tab) {
+		browser.tabs.reload(tab.id, {bypassCache: false});
+	});
+}
+
+function errorWarrantsRetry(error){
+	var valid_errors = ["NS_ERROR_UNKNOWN_HOST"];
+
+	if(valid_errors.indexOf(error) >= 0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+function errorCallback(status){
+	console.log("Error loading resource: "+status.url);
+	if(errorWarrantsRetry(status.error)){
+		setTimeout(function(){
+			retry(status.tabId);
+		}, 5000);
+	}
 }
 
 startup();
-browser.webRequest.onBeforeRequest.addListener(statusCallback, { urls: ["<all_urls>"] });
-console.log("still alive");
+browser.webRequest.onErrorOccurred.addListener(errorCallback, { urls: ["<all_urls>"] });
