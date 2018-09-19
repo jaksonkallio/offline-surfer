0f66553ef9c0c3e477a2704934e798aaeea6da41
Jakson Kallio <jakson@protonmail.com>
2018-09-19 11:39:24 -0500

    Offline surfer webRequest

---
+++ offline-surfer/background.js
@@ -0,0 +1,14 @@
+//var filter = { urls: ["<all_urls>"] };
+//browser.webRequest.onCompleted.addListener(statusCallback, filter);
+//browser.webRequest.onErrorOccurred.addListener(statusCallback, filter);
+
+function startup(){
+	console.log("Offline Surfer is active");
+}
+
+function statusCallback(status){
+	console.log(status.url);
+}
+
+startup();
+browser.webRequest.onBeforeRequest.addListener(statusCallback, { urls: ["<all_urls>"] });
